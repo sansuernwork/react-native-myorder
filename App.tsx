5fe136f996ca3a651896f823d6d9ebfb25b7ce50
sansuern muangsong <sansuern.work@gmail.com>
2023-10-19 14:52:34 +0700

    folder_structor

--- App.tsx
+++ App.tsx
@@ -1,26 +1,33 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
+import Home from './src/features/Home/Home';
+import Stock from './src/features/Stock/Stock';
+import {ReduxProvider} from './src/redux/store';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+type RootStackParamList = {
+  Home: undefined;
+  Stock: undefined;
+};
+
+type NavtiveProps = NativeStackScreenProps<RootStackParamList>;
 
 const Stack = createNativeStackNavigator();
-
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <ReduxProvider>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Stock" component={Stock} />
+        </Stack.Navigator>
+      </ReduxProvider>
     </NavigationContainer>
   );
 }
