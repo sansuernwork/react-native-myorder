d28182e9fc51898b43e45a7ca7ccefe999500f2c
sansuern muangsong <sansuern.work@gmail.com>
2023-10-19 18:47:10 +0700

    backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,37 @@
-import cors from "cors";
-import express, { Request, Response } from "express";
+import cors from 'cors';
+import express, {Request, Response} from 'express';
+import {PrismaClient} from '@prisma/client';
+import {Server} from 'http';
+import prisma from './src/client';
+
 const app = express();
 // parse json request body
 app.use(express.json());
 
 // parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 // enable cors
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Server is ok." });
+prisma.$connect().then(() => {
+  app.listen(3000, () => {
+    console.log(`Listening to port 3000`);
+  });
+});
+
+app.get('/api/ap', async (req: Request, res: Response) => {
+  const result = await prisma.user.findMany();
+  res.send(result);
+});
+
+app.get('/api/create', async (req: Request, res: Response) => {
+  const result = await prisma.user.create({
+    data: {
+      email: 'test',
+      name: 'test',
+    },
+  });
+  res.send(result);
 });
---
+++ backend/src/client.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+
+// add prisma to the NodeJS global type
+interface CustomNodeJsGlobal extends Global {
+  prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+const prisma = global.prisma || new PrismaClient({
+  // log: ['query', 'info', 'warn', 'error'],
+});
+
+prisma.$use(async (params, next) => {
+  const before = Date.now()
+  const result = await next(params)
+  const after = Date.now()
+  console.log(`Query ${params.model}.${params.action} took ${after - before}ms`)
+  return result
+});
+
+
+export default prisma;
